f4091dcd9825d3566080950fc7d029c5c6d9827a
raptor-2001 <pratham007yadav@gmail.com>
2023-01-25 17:11:19 +0530

    contextProvider added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDom from 'react-dom'
 
 import App from './App';
 
+import { ContextProvider } from './contexts/ContextProvider';
+
 import './index.css'
 
-ReactDom.render(<App/>, document.getElementById('root')); 
+ReactDom.render(
+  <ContextProvider>
+    <App/>
+  </ContextProvider>
+, document.getElementById('root')); 
