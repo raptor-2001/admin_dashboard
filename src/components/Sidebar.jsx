9eed6c039c5dd6bf8d0ef8a891473cb05221307f
raptor-2001 <pratham007yadav@gmail.com>
2023-01-25 09:32:51 +0530

    reverting the x icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,16 +20,15 @@ const Sidebar = () => {
       <>
         <div className="flex justify-between items-center">
           <Link to="/" onClick={()=>{}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-            <SiShopware /> <span>Shoppy</span>
+            <SiShopware /><span>Shoppy</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
             <button
               type="button"
               onClick={() => {}}
-
               className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
             >
-              <MdOutlineCancel />
+              <MdOutlineCancel/>
             </button>
           </TooltipComponent>
         </div>
